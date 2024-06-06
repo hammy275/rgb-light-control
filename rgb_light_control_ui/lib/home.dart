d66239b58e1ccdabe1064a6a1b2853948c82d443
hammy275 <hammy275@gmail.com>
2024-06-05 20:44:54 -0400

    Check State Before Starting Lights and Error Message if Invalid

--- rgb_light_control_ui/lib/home.dart
+++ rgb_light_control_ui/lib/home.dart
@@ -45,12 +45,35 @@ class HomeState extends State<Home> {
     });
   }
 
+  String areSettingsValid() {
+    /// Returns an empty string if settings are valid, or an error message if not.
+    if (selectedLights.isEmpty) {
+      return "At least one light must be selected.";
+    }
+    if (["Cycle to Music", "Fade with Music"].contains(selectedMode)) {
+      if (musicSettings.musicFile == null) {
+        return "A music file must be selected.";
+      }
+    }
+    if (selectedMode == "Fade with Music") {
+      if (musicSettings.colors.length != 2) {
+        return "Exactly two colors must be selected for 'Fade with Music'.";
+      }
+    } else if (selectedMode == "Cycle to Music") {
+      if (musicSettings.colors.length < 2) {
+        return "Please select at least two colors for 'Cycle to Music'.";
+      }
+    }
+    return "";
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Widget> actions = [];
     if (currentPageIndex == 0) {
       actions.add(IconButton(onPressed: () => setState(() => page0RefreshCounter++), icon: const Icon(Icons.refresh)));
     }
+    String cantStartError = areSettingsValid();
     return Scaffold(
       appBar: AppBar(title: const Text(Constants.title), backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       actions: actions),
@@ -62,31 +85,55 @@ class HomeState extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => showModalBottomSheet(context: context,
-            builder: (BuildContext context) {
-              List<String> lightNames = [];
-              for (final light in selectedLights) {
-                lightNames.add(light.name);
-              }
-              if (selectedMode == "Rainbow") {
-                return RainbowPlayback(lightNames: lightNames, settings: rainbowSettings);
-              } else {
-                return MusicPlayback(lightNames: lightNames, modeName: selectedMode, settings: musicSettings,);
-              }
+        builder: (BuildContext context) {
+          if (cantStartError.isNotEmpty) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(style: Theme.of(context).textTheme.titleLarge, cantStartError),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: FilledButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("Ok")
+                  )
+                )
+              ],
+            );
+          } else {
+            List<String> lightNames = [];
+            for (final light in selectedLights) {
+              lightNames.add(light.name);
+            }
+            if (selectedMode == "Rainbow") {
+              return RainbowPlayback(lightNames: lightNames, settings: rainbowSettings);
+            } else {
+              return MusicPlayback(lightNames: lightNames, modeName: selectedMode, settings: musicSettings,);
+            }
+          }
         }),
         label: const Text("Run Lights"),
         icon: const Icon(Icons.play_arrow)
       ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.lightbulb_outline), selectedIcon: Icon(Icons.lightbulb), label: "Lights"),
-          NavigationDestination(icon: Icon(Icons.settings_outlined), selectedIcon: Icon(Icons.settings), label: "Mode")
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          NavigationBar(
+            onDestinationSelected: (int index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.lightbulb_outline), selectedIcon: Icon(Icons.lightbulb), label: "Lights"),
+              NavigationDestination(icon: Icon(Icons.settings_outlined), selectedIcon: Icon(Icons.settings), label: "Mode")
+            ],
+            selectedIndex: currentPageIndex,
+          )
         ],
-        selectedIndex: currentPageIndex,
       ),
     );
   }
